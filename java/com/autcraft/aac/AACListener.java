8ca3bf1942c8a9f086554f3f7489a5149640c714
zayzn <mail@zayzn.de>
2024-10-02 16:26:05 +0200

    Optimize event handlers
    Make fail-early, improve compatibility, representative handler names.

--- java/com/autcraft/aac/AACListener.java
+++ java/com/autcraft/aac/AACListener.java
@@ -25,29 +25,27 @@ public class AACListener implements Listener {
      *
      * @param e
      */
-    @EventHandler
-    public void onClick(PlayerInteractEvent e) {
-        Player player = (Player) e.getPlayer();
+    @EventHandler(ignoreCancelled = true)
+    public void playerOpensInventoryGUI(PlayerInteractEvent e) {
+        if(e.getAction() == Action.PHYSICAL) return;
+
+        Player player = e.getPlayer();
         InventoryGUI inventoryGUI = plugin.getInventoryGUI();
         ItemStack item = player.getInventory().getItemInMainHand();
         ItemStack itemOffHand = player.getInventory().getItemInOffHand();
 
         // The AAC tool has to be in either the main hand or offhand to work
-        boolean validItem = inventoryGUI.isItemPanelTool(item) || inventoryGUI.isItemPanelTool(itemOffHand);
+        if(!inventoryGUI.isItemPanelTool(item) || !inventoryGUI.isItemPanelTool(itemOffHand)) return;
 
-        // Check to see if the item in the player's hand is the AAC Tool
-        // Make sure it's not a physical action, such as stepping onto a pressure plate
-        if (validItem && e.getAction() != Action.PHYSICAL) {
-            // If, for whatever reason, the player doesn't have permission to open the gui
-            if (!player.hasPermission("aac.open")) {
-                player.sendMessage(plugin.getString("error_no_permission"));
-                return;
-            }
-
-            player.openInventory(inventoryGUI.getGUI(player, 1));
-
-            e.setCancelled(true);
+        // If, for whatever reason, the player doesn't have permission to open the gui
+        if (!player.hasPermission("aac.open")) {
+            player.sendMessage(plugin.getString("error_no_permission"));
+            return;
         }
+
+        player.openInventory(inventoryGUI.getGUI(player, 1));
+
+        e.setCancelled(true);
     }
 
     /**
@@ -55,67 +53,66 @@ public class AACListener implements Listener {
      *
      * @param e
      */
-    @EventHandler
-    public void inventoryClick(InventoryClickEvent e) {
+    @EventHandler(ignoreCancelled = true)
+    public void playerMakesAACSelection(InventoryClickEvent e) {
 
         Component inventoryTitle = e.getView().title();
         Component GUITitle = Component.text(plugin.getConfig().getString("settings.title"));
 
-        // If the inventory clicked on is the same as what was created by AAC
-        if (inventoryTitle.equals(GUITitle)) {
-            // Disable all number key interactions when this menu is open. NO SWITCHING ITEMS!
-            if (e.getClick() == ClickType.NUMBER_KEY) {
-                e.setCancelled(true);
-                return;
-            }
+        if(!inventoryTitle.equals(GUITitle)) return;
 
-            // If the item clicked is not null and not air
-            if (e.getCurrentItem() != null && e.getCurrentItem().getType() != Material.AIR ) {
+        // Disable all number key interactions when this menu is open. NO SWITCHING ITEMS!
+        if (e.getClick() == ClickType.NUMBER_KEY
+                || e.getCurrentItem() == null
+                || e.getCurrentItem().getType() == Material.AIR
+        ) {
+            e.setCancelled(true);
+            return;
+        }
 
-                // Object vars
-                Player player = (Player) e.getWhoClicked();
-                ItemStack clickedItem = e.getCurrentItem();
-                InventoryGUI inventoryGUI = plugin.getInventoryGUI();
+        // Object vars
+        Player player = (Player) e.getWhoClicked();
+        ItemStack clickedItem = e.getCurrentItem();
+        InventoryGUI inventoryGUI = plugin.getInventoryGUI();
 
-                // If the next button is clicked
-                if (inventoryGUI.isNextButton(clickedItem)) {
-                    // Open GUI for the next page
-                    player.openInventory(inventoryGUI.getGUI(player, inventoryGUI.getNextPage(clickedItem)));
+        // If the next button is clicked
+        if (inventoryGUI.isNextButton(clickedItem)) {
+            // Open GUI for the next page
+            player.openInventory(inventoryGUI.getGUI(player, inventoryGUI.getNextPage(clickedItem)));
+        }
+        // If the previous button is clicked
+        else if (inventoryGUI.isPreviousButton(clickedItem)) {
+            // Open GUI for the previous page
+            player.openInventory(inventoryGUI.getGUI(player, inventoryGUI.getPreviousPage(clickedItem)));
+        }
+        // Otherwise, output to the chat
+        else {
+
+            // If it has persistent data, then it has the string to output
+            String output = inventoryGUI.getPersistentDataContainer(clickedItem);
+
+            // So long as the output isn't blank, send it to the chat
+            if (output != null) {
+                if (plugin.isInCooldown(player)) {
+                    HashMap<String, String> replacements = new HashMap<>();
+                    replacements.put("{SECONDS}", "" + plugin.getCooldownRemaining(player));
+
+                    player.sendMessage(plugin.errorMessage("error_player_in_cooldown", replacements));
+                } else {
+                    plugin.toConsole(player.getName() + " is using AAC to generate the following text in chat:");
+                    player.chat(output);
+
+                    // Message sent successfully, now apply cooldown
+                    plugin.addPlayerCooldown(player);
                 }
-                // If the previous button is clicked
-                else if (inventoryGUI.isPreviousButton(clickedItem)) {
-                    // Open GUI for the previous page
-                    player.openInventory(inventoryGUI.getGUI(player, inventoryGUI.getPreviousPage(clickedItem)));
-                }
-                // Otherwise, output to the chat
-                else {
-
-                    // If it has persistent data, then it has the string to output
-                    String output = inventoryGUI.getPersistentDataContainer(clickedItem);
-
-                    // So long as the output isn't blank, send it to the chat
-                    if (output != null) {
-                        if (plugin.isInCooldown(player)) {
-                            HashMap<String, String> replacements = new HashMap<>();
-                            replacements.put("{SECONDS}", "" + plugin.getCooldownRemaining(player));
-
-                            player.sendMessage(plugin.errorMessage("error_player_in_cooldown", replacements));
-                        } else {
-                            plugin.toConsole(player.getName() + " is using AAC to generate the following text in chat:");
-                            player.chat(output);
-
-                            // Message sent successfully, now apply cooldown
-                            plugin.addPlayerCooldown(player);
-                        }
-                    } else {
-                        plugin.toConsole("Error: No output was stored because the plugin could not set the persistent data for the panel option.");
-                    }
-                }
-
-                e.setCancelled(true);
-                player.updateInventory();
+            } else {
+                plugin.toConsole("Error: No output was stored because the plugin could not set the persistent data for the panel option.");
             }
         }
+
+        e.setCancelled(true);
+        player.updateInventory();
+
     }
 
     public boolean compareMaterials(Material material, String icon) {
