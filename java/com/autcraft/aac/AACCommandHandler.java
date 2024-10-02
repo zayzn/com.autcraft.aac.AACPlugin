2b9a98d031b3635cf8482933604754ed2ef45a9d
zayzn <mail@zayzn.de>
2024-10-02 17:02:54 +0200

    Clean up

--- java/com/autcraft/aac/AACCommandHandler.java
+++ java/com/autcraft/aac/AACCommandHandler.java
@@ -16,7 +16,7 @@ import java.util.*;
 public class AACCommandHandler implements CommandExecutor, TabCompleter {
     private final AACPlugin plugin;
 
-    public AACCommandHandler(AACPlugin plugin){
+    public AACCommandHandler(AACPlugin plugin) {
         this.plugin = plugin;
     }
 
@@ -24,22 +24,22 @@ public class AACCommandHandler implements CommandExecutor, TabCompleter {
     public boolean onCommand(@NotNull CommandSender commandSender, @NotNull Command command, @NotNull String s, @NotNull String[] args) {
 
         // No arguments provided - get help text
-        if( args.length == 0 ){
+        if (args.length == 0) {
             // Error: Invalid permission
-            if( !commandSender.hasPermission("aac.help") ) {
+            if (!commandSender.hasPermission("aac.help")) {
                 commandSender.sendMessage(plugin.errorMessage("error_no_permission"));
                 return true;
             }
 
-            commandSender.sendMessage(Component.text(plugin.getConfig().getString("settings.helptext")).color(TextColor.color(60, 180 ,180)));
+            commandSender.sendMessage(Component.text(plugin.getConfig().getString("settings.helptext")).color(TextColor.color(60, 180, 180)));
             return true;
         }
 
 
         // Reload the config and re-initalize the panel items
-        if( args[0].equalsIgnoreCase("reload") ){
+        if (args[0].equalsIgnoreCase("reload")) {
             // Error: Invalid permission
-            if( !commandSender.hasPermission("aac.reload") ) {
+            if (!commandSender.hasPermission("aac.reload")) {
                 commandSender.sendMessage(plugin.errorMessage("error_no_permission"));
                 return true;
             }
@@ -53,44 +53,42 @@ public class AACCommandHandler implements CommandExecutor, TabCompleter {
 
 
         // Get the knowledge book!
-        if( args[0].equalsIgnoreCase("get") ){
+        if (args[0].equalsIgnoreCase("get")) {
             // Error: Invalid permission
-            if( !commandSender.hasPermission("aac.get") ){
+            if (!commandSender.hasPermission("aac.get")) {
                 commandSender.sendMessage(plugin.errorMessage("error_no_permission"));
                 return true;
             }
 
             // If player
-            if( commandSender instanceof Player) {
-                Player player = (Player) commandSender;
+            if (commandSender instanceof Player player) {
                 InventoryGUI inventoryGUI = plugin.getInventoryGUI();
 
                 // Put the item into the player's inventory that will trigger the AAC GUI
                 player.getInventory().addItem(inventoryGUI.getTool());
                 plugin.debug("Gave AAC tool to " + player.getName());
-            }
-            else {
+            } else {
                 commandSender.sendMessage(plugin.errorMessage("error_no_console"));
                 return true;
             }
         }
 
         // If the player is trying to give the book to another player
-        if( args[0].equalsIgnoreCase("give") ){
+        if (args[0].equalsIgnoreCase("give")) {
             // Error: Invalid permission
-            if( !commandSender.hasPermission("aac.give") ) {
+            if (!commandSender.hasPermission("aac.give")) {
                 commandSender.sendMessage(plugin.errorMessage("error_no_permission"));
                 return true;
             }
 
             // Error: /aac give command ran but no player provided.
-            if( args.length == 1 ){
+            if (args.length == 1) {
                 commandSender.sendMessage(plugin.errorMessage("error_player_not_provided"));
                 return false;
             }
 
             // Error: /aac give <player> command ran but player is not online
-            if( plugin.getServer().getPlayer(args[1]) == null ){
+            if (plugin.getServer().getPlayer(args[1]) == null) {
                 commandSender.sendMessage(plugin.errorMessage("error_player_not_online"));
                 return true;
             }
@@ -118,31 +116,25 @@ public class AACCommandHandler implements CommandExecutor, TabCompleter {
 
     private List<String> onTabCompleteNoArgs(CommandSender sender) {
         var r = new TreeSet<String>();
-        if(sender instanceof Player) r.add("get");
+        if (sender instanceof Player) r.add("get");
         r.add("give");
         r.add("reload");
         return r.stream().toList();
     }
 
     private List<String> onTabCompleteArgs1(CommandSender sender, String[] args) {
-        if("give".equals(args[0])) {
-            return Bukkit.getOnlinePlayers().stream()
-                    .map(Player::getName)
-                    .toList();
+        if ("give".equals(args[0])) {
+            return Bukkit.getOnlinePlayers().stream().map(Player::getName).toList();
         }
         var options = onTabCompleteNoArgs(sender);
         return options.contains(args[0]) ? Collections.emptyList() : options;
     }
 
     private List<String> onTabCompleteArgs2(CommandSender sender, String[] args) {
-        if(!"give".equals(args[0])) {
+        if (!"give".equals(args[0])) {
             return Collections.emptyList();
         }
-        return Bukkit.getOnlinePlayers().stream()
-                .map(Player::getName)
-                .filter(s -> s.startsWith(args[1]))
-                .sorted()
-                .toList();
+        return Bukkit.getOnlinePlayers().stream().map(Player::getName).filter(s -> s.startsWith(args[1])).sorted().toList();
     }
 
 }
